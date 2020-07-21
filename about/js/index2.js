ff06e64680d3f58e98c4b4f79da429a5c3073362
dolphin-pro <67806342+dolphin-pro@users.noreply.github.com>
2020-07-21 17:18:35 +0300

    'p6'

--- about/js/index2.js
+++ about/js/index2.js
@@ -29,6 +29,34 @@ var tapSlider = document.getElementById('audio');
     }
   });
 
+  var mySwiper14 = new Swiper('.swiper-responsive-breakpoints', {
+    slidesPerView: 5,
+    spaceBetween: 50,
+    // init: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 40,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      }
+    }
+  });
+
 /*=========================================================================================
     File Name: fullcalendar.js
     Description: Fullcalendar
--- about/js/index3.js
+++ about/js/index3.js
@@ -29,6 +29,34 @@ var tapSlider = document.getElementById('audio');
     }
   });
 
+  var mySwiper14 = new Swiper('.swiper-responsive-breakpoints', {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    // init: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 1,
+        spaceBetween: 40,
+      },
+      768: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+      },
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      }
+    }
+  });
+
 /*=========================================================================================
     File Name: fullcalendar.js
     Description: Fullcalendar
