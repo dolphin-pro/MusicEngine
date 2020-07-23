7e1cf28f4ffff20081da88336663be71e7cdcf21
dolphin-pro <67806342+dolphin-pro@users.noreply.github.com>
2020-07-23 21:10:41 +0300

    'p15'

---
+++ discover/js/index.js
@@ -0,0 +1,17 @@
+$('.navbar-header, .main-menu').off('mouseenter').off('mouseleave');
+
+$('nav.header-navbar a.menu-handle').off('click');
+$('nav.header-navbar a.menu-handle').on('click',function (evt) {
+    var collapsed=$('body').hasClass('menu-collapsed');
+    if (collapsed) {
+        $('body').removeClass('menu-collapsed');
+        $('body').addClass('menu-expanded');
+        $('div.main-menu').addClass('expanded');
+        $(this).html('<i class="ficon feather icon-arrow-left"></i>');
+    }else{
+        $('body').addClass('menu-collapsed');
+        $('body').removeClass('menu-expanded');
+        $('div.main-menu').removeClass('expanded');
+        $(this).html('<i class="ficon feather icon-align-left"></i>');
+    }
+})
